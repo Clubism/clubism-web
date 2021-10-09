749d8eff512ba7dc39d30539ed084ccddafcf7aa
moong <moonki0623@naver.com>
2021-10-09 16:34:59 +0900

    [WIP]: URL FIX

--- src/Pages/MainClub.js
+++ src/Pages/MainClub.js
@@ -1,10 +1,11 @@
-import React,{useState, useEffect} from 'react';
-import IndexTemplate from '../Components/IndexTemplate';
-import MainClubPage from '../Components/MainClub/MainClubPage';
-import MainClubCategory from '../Components/MainClub/MainClubCategory';
+import React, { useState, useEffect } from "react";
+import IndexTemplate from "../Components/IndexTemplate";
+import MainClubPage from "../Components/MainClub/MainClubPage";
+import MainClubCategory from "../Components/MainClub/MainClubCategory";
+import "../Components/style/MainClubs.scss";
 
 const MainClub = (props) => {
-  const {params}=props.match;
+  const { params } = props.match;
   // console.log(params.category, params.name);
   const [mainCategory, setMainCategory] = useState({});
   const [category, setCategory] = useState("전체보기");
@@ -31,8 +32,7 @@ const MainClub = (props) => {
       />
       <MainClubPage category={params.category} name={params.name} />
     </IndexTemplate>
-
-  )
-}
+  );
+};
 
 export default MainClub;
