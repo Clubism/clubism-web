32865c783e7e9243772fd6369fbd76467ff88783
moong <moonki0623@naver.com>
2021-10-30 12:47:54 +0900

    [feat]: DetailClubPage style fixed

--- src/Components/DetailClub/DetailClubPage_test.js
+++ src/Components/DetailClub/DetailClubPage_test.js
@@ -8,70 +8,62 @@ import DetailClubQA from "./QA";
 import DetailClubIntro from "./Intro";
 
 const DetailClubPageTest = (props) => {
-    console.log(props);
-    const [selectedClub, setSelectedClub] = useState([]);
+  console.log(props);
+  const [selectedClub, setSelectedClub] = useState([]);
 
-    var fetchURL;
-    if (props.category === undefined)
-        fetchURL = "";
-    else if ((props.category !== undefined) && (props.name === undefined))
-        fetchURL = "../"
-    else
-        fetchURL = "../../"
+  var fetchURL;
+  if (props.category === undefined) fetchURL = "";
+  else if (props.category !== undefined && props.name === undefined)
+    fetchURL = "../";
+  else fetchURL = "../../";
 
-    useEffect(() => {
-        fetch(fetchURL + "dummy/mainclublist.json")
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    setSelectedClub(result.filter((data) => data.label === props.name));
-                    console.log(props.name, fetchURL)
-                },
-                (error) => {
-                    console.log(error);
-                    return;
-                }
-            );
-    }, [props.name, fetchURL]);
+  useEffect(() => {
+    fetch(fetchURL + "dummy/mainclublist.json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setSelectedClub(result.filter((data) => data.label === props.name));
+          console.log(props.name, fetchURL);
+        },
+        (error) => {
+          console.log(error);
+          return;
+        }
+      );
+  }, [props.name, fetchURL]);
 
-    var clubName = "";
-    if (selectedClub.length !== 0)
-        clubName = selectedClub[0].name;
+  var clubName = "";
+  if (selectedClub.length !== 0) clubName = selectedClub[0].name;
 
-
-    return (
-        <div className="detail-container">
-            {clubName !== "" ? (
-                <Tabs
-                    defaultActiveKey="profile"
-                    id="uncontrolled-tab-example"
-                    className="mb-3"
-                >
-                    <Tab
-                        className="detail-tab"
-                        eventKey={1}
-                        title={clubName}
-                        disabled
-                    >
-                        {/* <Sonnet /> */}
-                    </Tab>
-                    <Tab className="detail-tab" eventKey={2} title="동아리 소개">
-                        <DetailClubIntro />
-                    </Tab>
-                    <Tab className="detail-tab" eventKey={3} title="현재 모집 공고">
-                        <DetailClubCurrent />
-                    </Tab>
-                    <Tab className="detail-tab" eventKey={4} title="전체 모집 공고">
-                        <DetailClubAll />
-                    </Tab>
-                    <Tab className="detail-tab" eventKey={5} title="Q&A">
-                        <DetailClubQA />
-                    </Tab>
-                </Tabs>) : (
-                <div>none</div>
-            )}
-        </div>
-    );
+  return (
+    <div className="detail-container">
+      {clubName !== "" ? (
+        <Tabs
+          defaultActiveKey="2"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab className="detail-tab" eventKey={1} title={clubName} disabled>
+            {/* <Sonnet /> */}
+          </Tab>
+          <Tab className="detail-tab" eventKey={2} title="동아리 소개">
+            <DetailClubIntro />
+          </Tab>
+          <Tab className="detail-tab" eventKey={3} title="현재 모집 공고">
+            <DetailClubCurrent />
+          </Tab>
+          <Tab className="detail-tab" eventKey={4} title="전체 모집 공고">
+            <DetailClubAll />
+          </Tab>
+          <Tab className="detail-tab" eventKey={5} title="Q&A">
+            <DetailClubQA />
+          </Tab>
+        </Tabs>
+      ) : (
+        <div>none</div>
+      )}
+    </div>
+  );
 };
 
 export default DetailClubPageTest;
