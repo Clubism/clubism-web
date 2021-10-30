76b3ab426780d8805821f8d58ad0f2af57432cb1
moong <moonki0623@naver.com>
2021-10-30 12:47:29 +0900

    [WIP]: Detail_Current page WIP

--- src/Components/DetailClub/Current.js
+++ src/Components/DetailClub/Current.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import "../style/DetailCurrent.scss";
 
 const DetailClubCurrent = () => {
-    return (
-        <div>
-            현재 모집 공고
+  return (
+    <>
+      <div className="CurrentContainer">
+        <div className="HorContainer">
+          <div className="CurrentPoster">
+            <img
+              src={require("../../Assets/Image/sgaem/sgaem_2019.png").default}
+              style={{ width: "200px", height: "280px" }}
+              alt="current_poster"
+            />
+          </div>
+          {/* *********** */}
+          <div className="CurrentMain">asdfasdasfd</div>
         </div>
-    )
+        <div>동아리 소개</div>
+      </div>
+    </>
+  );
 };
 
 export default DetailClubCurrent;
