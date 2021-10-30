2ab081a13079669e4a6df75fa728f10cb0480a2c
moong <moonki0623@naver.com>
2021-10-30 10:52:43 +0900

    [fix]: pages/MainClub.js bug fix

--- src/Pages/MainClub.js
+++ src/Pages/MainClub.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import IndexTemplate from '../Components/IndexTemplate';
+import React from "react";
+import IndexTemplate from "../Components/IndexTemplate";
 // import MainClubPage from '../Components/MainClub/MainClubPage';
 // import MainClubCategory from '../Components/MainClub/MainClubCategory';
-import MainClubCategoryTest from '../Components/MainClub/MainClubCategory_test';
-import MainClubPageTest from '../Components/MainClub/MainClubPage_test';
+import MainClubCategoryTest from "../Components/MainClub/MainClubCategory_test";
+import MainClubPageTest from "../Components/MainClub/MainClubPage_test";
 import "../Components/style/MainClub.scss";
 
 const MainClub = (props) => {
@@ -12,12 +12,13 @@ const MainClub = (props) => {
 
   return (
     <div>
-      <IndexTemplate>
-      </IndexTemplate>
+      <IndexTemplate />
       <div className="mainClubContainer">
         <MainClubCategoryTest category={params.category} name={params.name} />
         <MainClubPageTest category={params.category} name={params.name} />
       </div>
     </div>
+  );
+};
 
 export default MainClub;
