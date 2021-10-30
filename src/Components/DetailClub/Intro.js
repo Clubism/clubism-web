7c2083012375ab040bd707f8a06cc716f49ce704
moong <moonki0623@naver.com>
2021-10-30 12:47:03 +0900

    [feat]: Detail_Intro style fixed

--- src/Components/DetailClub/Intro.js
+++ src/Components/DetailClub/Intro.js
@@ -6,11 +6,22 @@ const DetailClubIntro = () => {
   return (
     <div className="IntroContainer">
       <div className="IntroPoster">
-        <img src="" alt="poster" />
+        <img
+          src={require("../../Assets/Image/sgaem/sgaem_2019.png").default}
+          style={{ width: "200px", height: "280px" }}
+          alt="poster"
+        />
       </div>
       <div className="IntroMain">
         <Carousel variant="dark" style={{ height: "280px" }}>
-          <Carousel.Item>asdf</Carousel.Item>
+          <Carousel.Item>
+            {/* <img
+              src={require("../../Assets/Image/sgaem/sgaem_2019.png").default}
+              style={{ width: "310px" }}
+              alt="img1"
+            /> */}
+            asdf
+          </Carousel.Item>
           <Carousel.Item>qwer</Carousel.Item>
           <Carousel.Item>zxcv</Carousel.Item>
         </Carousel>
