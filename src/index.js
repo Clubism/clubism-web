d8b34884cb8cc2dbdd7621b3a26fcb30010fd463
chyeuni <chyeuni99@naver.com>
2021-11-17 09:43:49 +0900

    [fix]:git init

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
-    <BrowserRouter>
-     <React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-     </React.StrictMode>
-    </BrowserRouter>,
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
