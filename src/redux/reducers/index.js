6f6ba7925b3983df75d6b92f4cb7c4ac046c67b8
강진실 <jinsil@jinsil-ui-MacBookAir.local>
2021-12-01 22:28:07 +0900

    wow

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 
 import category from './category';
+import post from './post'
+import auth from './auth'
 
 const rootReducer = combineReducers({
     category,
+    post,
+    auth,
 });
 
 export default rootReducer;
