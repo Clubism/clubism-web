ce8c16e1fab993b5958d2d558405d2ef195e4887
kauthenticity <kauthenticity@gmail.com>
2022-01-02 23:22:01 +0900

    [feat] my page 추가

--- src/Components/MyPage/FavKeywords.js
+++ src/Components/MyPage/FavKeywords.js
@@ -6,40 +6,71 @@ const FavKeywords = ()=> {
   return (
     <FavKeywordsWrapper>
       <FavKeywordsHeader>관심 키워드 수정</FavKeywordsHeader>
+
       <FavKeywordsContainer>
         
       </FavKeywordsContainer>
+
+      <FavKeywordsFooter>
+        <Submit>확인</Submit>
+        <Cancel>취소</Cancel>
+      </FavKeywordsFooter>
       
     </FavKeywordsWrapper>
     );
 };
 
 const FavKeywordsWrapper = styled.div`
-  display : flex;
+  display : block;
   width : 900px;
   margin : auto;
-  margin-top : 100px;
-  
+  margin-top : 40px;
   color : #333333;
-
-  //border : solid;
+  padding : 0px 100px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 `;
 
 const FavKeywordsHeader = styled.div`
   height : 100px;
-  
-  width : 100%;
+  display : flex;
+  width : 900px;
   font-size : 24px;
-  font-weight : 600;
-  padding : 20px;
-
+  font-weight : 500;
+  padding : 40px 80px;
+  align-items : center;
+  background-color: #013B6C;
+  color : white;
+  transform : translateX(-100px);
   //border : solid;
 `;
 
 const FavKeywordsContainer = styled.div`
-  height : 800px;
+ 
+`;
+
+const FavKeywordsFooter = styled.div`
   display : flex;
-  
+  font-size : 14px;
+  justify-content: flex-end;
+  padding : 20px 0px;
+`;
+
+const Submit = styled.button`
+  border-style : none;
+  border-radius : 2px;
+  padding : 12px 72px;
+  margin : 0px 20px;
+  background-color : #013B6C;
+  color : white;
+`;
+const Cancel = styled.button`
+  border-style : solid;
+  border-width : thin;
+  border-radius : 2px;
+  padding : 12px 72px;
+  background-color : white;
+  border-color : #dadada;
+  color : #013B6C;
 `;
 
 export default FavKeywords;
